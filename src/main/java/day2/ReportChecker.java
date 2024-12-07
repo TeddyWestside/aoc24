e9bd019a9aee32965b11f00ff2ad530c3116f175
Carsten Mußenbrock <teddywestside@users.noreply.github.com>
2024-12-07 17:45:01 +0100

    day 2 - part 1

--- src/main/java/day2/ReportChecker.java
+++ src/main/java/day2/ReportChecker.java
@@ -3,29 +3,57 @@ package day2;
 import utils.SharedFunctions;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReportChecker {
+    private boolean hasProblemDampener;
+
+    public ReportChecker(boolean hasProblemDampener) {
+        this.hasProblemDampener = hasProblemDampener;
+    }
+
     public int calculateSafeReports(String path) {
         int safeReportsCount = 0;
         var myReader = SharedFunctions.getScanner(path);
         while (myReader.hasNextLine()) {
             String line = myReader.nextLine();
-            int[] numbers = Arrays.stream(line.trim().split("\\s+"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            if (hasDistanceGreaterThan3(numbers))
+            List<Integer> numbers = Arrays.stream(line.trim().split("\\s+"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            if (hasDistanceSmallerThan4(numbers) && (isConstantlyIncreasing(numbers) || isConstantlyDecreasing(numbers))) {
                 safeReportsCount++;
+            }
         }
         return safeReportsCount;
     }
 
-    private boolean hasDistanceGreaterThan3(int[] numbers) {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            if (Math.abs(numbers[i] - numbers[i + 1]) > 3) {
-                return true;
+    private boolean isConstantlyDecreasing(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) <= numbers.get(i + 1)) {
+                return false;
             }
         }
-        return false;
+        return true;
+    }
+
+
+    private boolean isConstantlyIncreasing(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) >= numbers.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasDistanceSmallerThan4(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (Math.abs(numbers.get(i) - numbers.get(i + 1)) > 3) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
--- src/test/java/Day2Test.java
+++ src/test/java/Day2Test.java
@@ -5,15 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day2Test {
 
+    // Part 1
     @Test
     public void shouldMeasureDistanceWithTestData() {
-        ReportChecker reportChecker = new ReportChecker();
+        ReportChecker reportChecker = new ReportChecker(false);
         assertEquals(2, reportChecker.calculateSafeReports("src/test/resources/day2/test.txt"));
     }
 
     @Test
     public void shouldMeasureDistanceWithPuzzleData() {
-        ReportChecker reportChecker = new ReportChecker();
-        assertEquals(2, reportChecker.calculateSafeReports("src/main/resources/day2/list.txt"));
+        ReportChecker reportChecker = new ReportChecker(false);
+        assertEquals(686, reportChecker.calculateSafeReports("src/main/resources/day2/list.txt"));
+    }
+
+    // Part 2
+    @Test
+    public void shouldMeasureDistanceWithTestDataAndProblemDampener() {
+        ReportChecker reportChecker = new ReportChecker(true);
+        assertEquals(4, reportChecker.calculateSafeReports("src/test/resources/day2/test2.txt"));
     }
 }
