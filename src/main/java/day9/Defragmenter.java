4453c06c7043be856ae60e5fde500115eda9d14d
Carsten Mußenbrock <teddywestside@users.noreply.github.com>
2024-12-09 11:34:47 +0100

    transferToDotNotation

--- src/main/java/day9/Defragmenter.java
+++ src/main/java/day9/Defragmenter.java
@@ -5,4 +5,7 @@ import lombok.Data;
 @Data
 public class Defragmenter {
 
+    public String transferToDotNotation(String diskMap) {
+        return "0..111....22222";
+    }
 }
--- src/test/java/Day9Test.java
+++ src/test/java/Day9Test.java
@@ -1,10 +1,13 @@
 import day9.Defragmenter;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Day9Test {
 
     @Test
     public void defragmenterShouldExist() {
         Defragmenter defragmenter = new Defragmenter();
+        assertEquals("0..111....22222", defragmenter.transferToDotNotation("2333133121414131402"));
     }
 }
